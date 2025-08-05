24fa6df330dbb408efbe891377a2fd63402cef91
Fadel Matar <fadel.matar@gmail.com>
2025-08-05 00:40:06 -0400

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use app\Models\Student;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [StudentController::class, 'index']);
+Route::resource('students', StudentController::class);
+
+Route::get(
+    'students/trash/{id}',
+    [StudentController::class,'trash']
+)->name('students.trash');
+
+Route::get(
+ 'students/trashed/',
+    [StudentController::class, 'trashed']
+    )->name('students.trashed');
+
+
+
+Route::get(
+    'students/restore/{id}',
+    [StudentController::class, 'restore']
+    )->name('students.restore');
+
